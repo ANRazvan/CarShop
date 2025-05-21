76ad7934777290b70ecda587201d78ff005aa95e
ANRazvan <alexe.razvan05@gmail.com>
2025-05-22 00:22:01 +0300

    fix car ?

--- CarShopBackend/models/Car.js
+++ CarShopBackend/models/Car.js
@@ -1,65 +1,31 @@
-// filepath: CarShopBackend/models/Car.js
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/supabase-db'); // Make sure this exports a Sequelize instance!
+const getConnection = require('../db');
 
-const Car = sequelize.define('Car', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  make: { 
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  model: { 
-    type: DataTypes.STRING, 
-    allowNull: false 
-  },
-  year: { 
-    type: DataTypes.INTEGER, 
-    allowNull: false 
-  },
-  keywords: { 
-    type: DataTypes.STRING 
-  },
-  description: { 
-    type: DataTypes.TEXT 
-  },
-  fuelType: { 
-    type: DataTypes.STRING, 
-    allowNull: false 
-  },
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: true,
-    references: {
-      model: 'Users',
-      key: 'id'
-    }
-  },
-  price: { 
-    type: DataTypes.DECIMAL(10, 2), 
-    allowNull: false 
-  },
-  img: { 
-    type: DataTypes.TEXT('long'),
-    comment: 'Base64 encoded image data'
-  },
-  imgType: {
-    type: DataTypes.STRING,
-    defaultValue: 'image/jpeg'
-  },
-  brandId: {
-    type: DataTypes.INTEGER,
-    allowNull: true,
-    references: {
-      model: 'Brands',
-      key: 'id'
-    }
-  }
-}, {
-  timestamps: true
-});
+async function defineCarModel() {
+  const sequelize = await getConnection();
+  
+  return sequelize.define('Car', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    make: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    model: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    year: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, {
+    tableName: 'cars',
+    timestamps: false,
+  });
+}
 
-module.exports = Car;
+module.exports = defineCarModel;
