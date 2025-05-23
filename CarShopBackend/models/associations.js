916231a1fbd12acf992fb54e983eb137ed8d4ac1
ANRazvan <alexe.razvan05@gmail.com>
2025-05-23 20:05:07 +0300

    fixed brand.findAll

--- CarShopBackend/models/associations.js
+++ CarShopBackend/models/associations.js
@@ -1,67 +1,63 @@
-const { getBrand } = require('./Brand');
-const { getCar } = require('./Car');
+// filepath: CarShopBackend/models/associations.js
+const Brand = require('./Brand');
+const Car = require('./Car');
 const User = require('./User');
 const UserLog = require('./UserLog');
 const MonitoredUser = require('./MonitoredUser');
 
 // Define associations
-async function setupAssociations() {
-    const Brand = getBrand();
-    const Car = getCar();
-    
-    // One Brand has many Cars
-    Brand.hasMany(Car, { 
-        foreignKey: 'brandId',
-        as: 'cars',
-        onDelete: 'CASCADE'
-    });
-    
-    // Car belongs to one Brand
-    Car.belongsTo(Brand, { 
-        foreignKey: 'brandId',
-        as: 'brand'
-    });
+const setupAssociations = () => {
+  // One Brand has many Cars
+  Brand.hasMany(Car, { 
+    foreignKey: 'brandId',
+    as: 'cars',
+    onDelete: 'CASCADE'
+  });
+  
+  // Car belongs to one Brand
+  Car.belongsTo(Brand, { 
+    foreignKey: 'brandId',
+    as: 'brand'
+  });
 
-    // User has many Logs
-    User.hasMany(UserLog, {
-        foreignKey: 'userId',
-        as: 'logs'
-    });
-    
-    // Log belongs to a User
-    UserLog.belongsTo(User, {
-        foreignKey: 'userId',
-        as: 'user'
-    });
+  // User has many Logs
+  User.hasMany(UserLog, {
+    foreignKey: 'userId',
+    as: 'logs'
+  });
+  
+  // Log belongs to a User
+  UserLog.belongsTo(User, {
+    foreignKey: 'userId',
+    as: 'user'
+  });
 
-    // User has many monitored entries
-    User.hasMany(MonitoredUser, {
-        foreignKey: 'userId',
-        as: 'monitoringEvents'
-    });
-    
-    // Monitored entry belongs to a User
-    MonitoredUser.belongsTo(User, {
-        foreignKey: 'userId',
-        as: 'user'
-    });
-    // Add ownership relationship for User to Cars
-    User.hasMany(Car, {
-        foreignKey: 'userId',
-        as: 'cars'
-    });
-      // Car belongs to a User
-    Car.belongsTo(User, {
-        foreignKey: 'userId',
-        as: 'owner'
-    });
-    
-    Car.belongsTo(User, {
-        foreignKey: 'userId',
-        as: 'user'
-    });
-}
-
-module.exports = {
-    setupAssociations
+  // User has many monitored entries
+  User.hasMany(MonitoredUser, {
+    foreignKey: 'userId',
+    as: 'monitoringEvents'
+  });
+  
+  // Monitored entry belongs to a User
+  MonitoredUser.belongsTo(User, {
+    foreignKey: 'userId',
+    as: 'user'
+  });
+  // Add ownership relationship for User to Cars
+  User.hasMany(Car, {
+    foreignKey: 'userId',
+    as: 'cars'
+  });
+    // Car belongs to a User
+  Car.belongsTo(User, {
+    foreignKey: 'userId',
+    as: 'owner'
+  });
+  
+  Car.belongsTo(User, {
+    foreignKey: 'userId',
+    as: 'user'
+  });
 };
+
+module.exports = setupAssociations;
