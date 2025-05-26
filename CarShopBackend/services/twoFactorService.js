e21a48a0e536664264f0ff1dd1a844f777e59979
ANRazvan <alexe.razvan05@gmail.com>
2025-05-26 21:13:06 +0300

    2FA 1.1

--- CarShopBackend/services/twoFactorService.js
+++ CarShopBackend/services/twoFactorService.js
@@ -2,69 +2,8 @@ const speakeasy = require('speakeasy');
 const QRCode = require('qrcode');
 const crypto = require('crypto');
 
-const twoFactorService = {
-    // Generate a new secret for a user
-    generateSecret: function(email) {
-        const secret = speakeasy.generateSecret({
-            name: `CarShop (${email})`,
-            issuer: 'CarShop'
-        });
-        return secret;
-    },
-
-    // Generate QR code for the secret
-    generateQRCode: async function(secret) {
-        try {
-            const qrCodeUrl = await QRCode.toDataURL(secret.otpauth_url);
-            return qrCodeUrl;
-        } catch (error) {
-            throw new Error('Failed to generate QR code');
-        }
-    },
-
-    // Verify TOTP token
-    verifyToken: function(token, secret) {
-        try {
-            return speakeasy.totp.verify({
-                secret: secret.base32,
-                encoding: 'base32',
-                token: token,
-                window: 1 // Allow 30 seconds window
-            });
-        } catch (error) {
-            return false;
-        }
-    }
-
-    // Generate backup codes
-    generateBackupCodes() {
-        const codes = [];
-        for (let i = 0; i < 10; i++) {
-            codes.push(crypto.randomBytes(4).toString('hex'));
-        }
-        return codes;
-    }
-
-    // Verify backup code
-    verifyBackupCode(userBackupCodes, providedCode) {
-        const codes = JSON.parse(userBackupCodes);
-        const index = codes.indexOf(providedCode);
-        if (index !== -1) {
-            codes.splice(index, 1); // Remove used code
-            return {
-                isValid: true,
-                remainingCodes: codes
-            };
-        }
-        return {
-            isValid: false,
-            remainingCodes: codes
-        };
-    }
-}
-
 module.exports = {
-    generateSecret: function(email) {
+    generateSecret(email) {
         const secret = speakeasy.generateSecret({
             name: `CarShop (${email})`,
             issuer: 'CarShop'
@@ -72,7 +11,7 @@ module.exports = {
         return secret;
     },
 
-    generateQRCode: async function(secret) {
+    async generateQRCode(secret) {
         try {
             const qrCodeUrl = await QRCode.toDataURL(secret.otpauth_url);
             return qrCodeUrl;
@@ -81,7 +20,7 @@ module.exports = {
         }
     },
 
-    verifyToken: function(token, secret) {
+    verifyToken(token, secret) {
         try {
             return speakeasy.totp.verify({
                 secret: secret.base32,
@@ -94,7 +33,7 @@ module.exports = {
         }
     },
 
-    generateBackupCodes: function() {
+    generateBackupCodes() {
         const codes = [];
         for (let i = 0; i < 10; i++) {
             codes.push(crypto.randomBytes(4).toString('hex'));
@@ -102,7 +41,7 @@ module.exports = {
         return codes;
     },
 
-    verifyBackupCode: function(userBackupCodes, providedCode) {
+    verifyBackupCode(userBackupCodes, providedCode) {
         try {
             const codes = JSON.parse(userBackupCodes);
             const index = codes.indexOf(providedCode);
